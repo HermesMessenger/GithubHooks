52badddd6f0a969c1ad72a937d5173350b1fbaaa
VladyslavLukyanenko <me@vladyslav.xyz>
2018-10-26 14:06:16 +0200

    Innit

---
+++ github.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');;
+const app = express();
+var execFile = require('child_process').execFile;
+app.use(bodyParser.json());
+////
+const port = 3000;
+var repo = 'Codernauts/social-network';
+var branch = 'master';
+
+app.post('/', function(req, res) {
+    var json = req.body;
+    var branch_ = json.ref.split('/').pop();
+    var repo_ = json.repository.full_name;
+    if(repo_ == repo && branch_ == branch)
+    {
+         console.log("OK");
+         res.status(200).send('OK');
+         execFile('./hook.sh', function(error, stdout, stderr) {
+         console.log('Ejecutado');
+      });
+    }
+    else
+    {
+        res.status(500).send('KO');  
+    }
+});
+
+app.listen(port, () => console.log(`Github Hooks on port ${port}!`))
---
+++ hook.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wget -O projectmaster.zip -q https://github.com/Codernauts/social-network/archive/master.zip
+
+if [ -f projectmaster.zip ]; then
+
+    unzip -q projectmaster.zip
+
+    rm projectmaster.zip
+
+    rm -rf Produccion-Social
+
+    mv social-network-master Produccion-Social
+    
+    chmod +x ./Produccion-Social/start.sh
+
+    kill -9 `ps -edaf|grep -i "start.sh"|grep -v "grep"|cut -d" " -f4`
+    
+    nohup ./Produccion-Social/start.sh > trazas.log &
+    
+    echo "Started :)"
+fi
