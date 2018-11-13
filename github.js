d3207a71e70e99fec3421b021cbc192c1e224fe9
spaceface777 <spaceface777@github.com>
2018-11-13 20:23:07 +0100

    Add two servers, one for each branch

--- github.js
+++ github.js
@@ -1,72 +1,57 @@
-function getNow() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return day + "/" + month + "/" + year + "$" + hour + ":" + min + ":" + sec;
-
-}
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const redis = require("redis").createClient();
+const utils = require('./utils');
 const app = express();
 const execFile = require('child_process').execFile;
 const port = 3000;
 const SEPCHAR = String.fromCharCode(0x1);
 
-
 app.use(bodyParser.json());
 
+console.log('---------------------------------------------')
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
 
     var json = req.body;
 
     var repo = json.repository.name;
     var branch = json.ref.split('/').pop();
-    var commitID = (json.head_commit.id).substring(0,7);
-    var commitMessage = json.head_commit.message;
-    commitMessage = commitMessage.split('\n')[0];
+    var commitID = (json.head_commit.id).substring(0, 7);
+    var commitMessage = (json.head_commit.message).split('\n')[0];
+    var commiterName = json.head_commit.author.username;
+
 
     if (repo == 'Hermes') {
         if (branch == 'master') {
             console.log("Received hook in normal branch");
             res.status(200).send('OK');
-            console.log ('Updating server...')
-
-            redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Server is updating...' + SEPCHAR + getNow());
-
-            execFile("./hook.sh", function(error, stdout, stderr) {
+            console.log('Updating server...')
+            redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Normal server is updating...' + SEPCHAR + utils.getNow());
+            execFile("./hook.sh", function () {
                 console.log('Server updated to commit ' + commitID);
-                redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Server updated to commit #' + commitID + ' - ' + commitMessage + SEPCHAR + getNow());
-                redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Please reload the page in order to access the new version.' + SEPCHAR + getNow())
+                redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Normal server updated to commit #' + commitID + ' by @' + commiterName +' - ' + commitMessage + SEPCHAR + utils.getNow());
+                redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Please reload the page in order to access the new version.' + SEPCHAR + utils.getNow())
             });
 
+        } else if (branch == 'testing') {
+            console.log("Received hook in testing branch");
+            res.status(200).send('OK');
+            console.log('Updating testing server...')
+            redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Testing server is updating...' + SEPCHAR + utils.getNow());
+            execFile("./hook-testing.sh", function () {
+                console.log('Testing server updated to commit ' + commitID);
+                redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Testing server updated to commit #' + commitID + ' by @' + commiterName +' - ' + commitMessage + SEPCHAR + utils.getNow());
+                redis.lpush('messages', 'Admin Bot' + SEPCHAR + 'Please access that page ( https://hermesmessenger-testing.duckdns.org ) in order to see the new version.' + SEPCHAR + utils.getNow())
+            });
         } else {
-            res.status(500).send('Wrong branch');
+            res.status(304).send('Wrong branch');
         };
 
     } else {
-        res.status(500).send('Wrong repo');
+        res.status(409).send('Wrong repo');
     }
 });
 
-app.listen(port, () => console.log(`Github Hooks on port ${port}!`))
+app.listen(port, () => console.log('Running GitHub Webhooks on port ' + port + '.'))
 
---
+++ hook-testing.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+branch='testing'
+
+wget -O projectmaster.zip -q "https://github.com/Codernauts/Hermes/archive/$branch.zip"
+
+target_dir="Produccion-Social-Testing"
+
+if [ -f projectmaster.zip ]; then
+
+    unzip -q projectmaster.zip
+
+    rm projectmaster.zip
+
+    (cd "Hermes-$branch"; npm i)
+
+    pm2 stop "Hermes Server Testing"
+
+    rm -rf $target_dir
+
+    mv "Hermes-$branch" $target_dir
+    sed -i 's/8080/8081/g' "$target_dir/server.js"
+
+    pm2 start "Hermes Server Testing"
+
+    echo "Started :)"
+fi
