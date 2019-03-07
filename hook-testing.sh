19ad6edaaa4e8462a9dc69107feb69645ec29d0a
spaceface777 <spaceface7777@gmail.com>
2019-03-07 18:33:08 +0100

    Fix

--- hook-testing.sh
+++ hook-testing.sh
@@ -21,7 +21,7 @@ if [ -f projectmaster.zip ]; then
     mv "Hermes-$branch" $target_dir
 
     cp config-testing.json $target_dir/config.json
-    cp assetLinks.json $target_dir/web_client/assetLinks.json
+    cp assetLinks-testing.json $target_dir/web_client/assetLinks.json
 
     pm2 start "Testing"
 
